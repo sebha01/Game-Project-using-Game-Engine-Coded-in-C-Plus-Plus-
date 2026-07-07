c4e5c024f5dc0eebf131f7fa2b49279f2321f052
sebha01 <30077548@students.southwales.ac.uk>
2026-07-07 14:02:31 +0100

    Completed more work on the window class, set up renderer class files

--- Mizu Engine/Window/MizuEngineWindow.cpp
+++ Mizu Engine/Window/MizuEngineWindow.cpp
@@ -1,5 +1,20 @@
 #include "MizuEngineWindow.h"
 
+//CALLBACK FUNCTIONS
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+//Window Input functions
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+//WINDOW CLASS FUNCTIONS
+
 MizuEngineWindow::MizuEngineWindow()
 {
 }
@@ -19,6 +34,8 @@ void MizuEngineWindow::WindowInit()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//Include for mac os
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	//Create window and set context
 
@@ -27,28 +44,47 @@ void MizuEngineWindow::WindowInit()
 	mode = glfwGetVideoMode(monitor);
 
 	window = glfwCreateWindow(mode->width * 0.66, mode->height * 0.66, "Mizu Engine v1.0.0", NULL, NULL);
-	//glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+	
+	//Register callback function 
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//set window as the current context
 	glfwMakeContextCurrent(window);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
-		std::cout << "Couldn't load opengl" << std::endl;
+		std::cout << "Couldn't load opengl, failed to initialise GLAD." << std::endl;
 		glfwTerminate();
 		return;
 	}
 
+	//Set screen colour
 	glClearColor(0.08f, 0.12f, 0.35f, 1.0f);
+
+	//Set viewport
+	glViewport(0, 0, mode->width * 0.66, mode->height * 0.66);
+
 }
 
 void MizuEngineWindow::WindowUpdate()
 {
+	//render loop
 	while (!glfwWindowShouldClose(window))
 	{
+		//input
+		processInput(window);
+
+		//Clear colour from last frame
+		glClearColor(0.08f, 0.12f, 0.35f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//rendering loop here
+
+		//check and call events and swap buffers
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwSwapBuffers(window);
 	}
 }
 
